b6cbe80ff312585f75af5ec3157e426af6953567
Robin Gautam <rbngtm1@gmail.com>
2018-06-14 20:13:39 -0400

    myfirstMVC

---
+++ myfirstMVC/firstMVC/Controllers/SummerController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using firstMVC.Models;
+
+namespace firstMVC.Controllers
+{
+    public class SummerController : Controller
+    {
+        // GET: Summer
+        public ActionResult Index()
+        {
+            List<Summer> summerList = new List<Summer>();
+
+            Summer summer = new Models.Summer();
+            summer.SummerEvent = "Italian Festival";
+            summer.SummerLocation = "Wilmington, DE";
+            summer.SummerYear = 2018;
+            summer.Active = true;
+            summer.ImageURL = "/Content/Images/italian.jpg";
+            summerList.Add(summer);
+
+            summer = new Models.Summer();
+            summer.SummerEvent = "Greek Festival";
+            summer.SummerLocation = "Wilmington, DE";
+            summer.SummerYear = 2018;
+            summer.Active = true;
+            summer.ImageURL = "/Content/Images/greek.jpg";
+            summerList.Add(summer);
+
+            summer = new Models.Summer();
+            summer.SummerEvent = "Polish Festival";
+            summer.SummerLocation = "Wilmington, DE";
+            summer.SummerYear = 2018;
+            summer.Active = true;
+            summer.ImageURL = "/Content/Images/polish.jpg";
+            summerList.Add(summer);
+
+
+            return View(summerList);
+        }
+    }
+}
---
+++ myfirstMVC/firstMVC/Models/Summer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace firstMVC.Models
+{
+    public class Summer
+    {
+        public string SummerEvent { get; set; } = "Greek Festival";
+        public string SummerLocation { get; set; }
+        public int SummerYear { get; set; }
+        public bool Active { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
